350f530a5e77f1d10e92ea415a5b71cea7539df3
Riccardo Bellini <101564706+Riccardo549@users.noreply.github.com>
2022-05-03 21:48:01 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Riccardo/CLionProjects/ElaboratoProgrammazione2/lib/googletest/googletest
-# Build directory: /Users/Riccardo/CLionProjects/ElaboratoProgrammazione2/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/src/CMakeFiles/ElaboratoProgrammazione2_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libElaboratoProgrammazione2_lib.a"
-)
