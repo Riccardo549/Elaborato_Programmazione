2e307fd609be10194a5323dd68657eec660795af
Riccardo <riccardo.bellini@stud.unifi.it>
2022-05-03 19:18:53 +0200

    SetUp fixture aggiunto

--- tst/TestMessage.cpp
+++ tst/TestMessage.cpp
@@ -6,12 +6,25 @@
 #include "Message.h"
 #include "User.h"
 
+class TestMessage: public::testing::Test{
+public:
+    virtual void SetUp(){
+        f = std::make_shared<User>(User("Francesco"));
+        a = std::make_shared<User>(User("Annalisa"));
+        b = std::make_shared<User>(User("Benedetta"));
+        m = std::make_shared<Message>(Message(f->getName(),a->getName(),"Ciao"));
+    }
 
-TEST(Message, Message_GetterSetter_Test) {
-    auto f = std::make_shared<User>(User("Francesco"));
-    auto a = std::make_shared<User>(User("Annalisa"));
-    auto b = std::make_shared<User>(User("Benedetta"));
-    auto m = std::make_shared<Message>(Message(f->getName(),a->getName(),"Ciao"));
+    std::shared_ptr<User> f;
+    std::shared_ptr<User> a;
+    std::shared_ptr<User> b;
+    std::shared_ptr<Message> m;
+
+
+};
+
+
+TEST_F(TestMessage, Message_GetterSetter_Test) {
     m->setSender(b->getName());
     ASSERT_EQ(m->getSender(),"Benedetta");
     m->setRead(true);
